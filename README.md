2ec299e9e33c6e5d14f8d59c8d3b9744549e32b3
yubo <yubo@xiaomi.com>
2017-09-30 12:03:36 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# falcon ui vue
+
+这是一个废弃的项目，用来做 [open-falcon](http://open-falcon.org/) 的页面展现，新版会使用 [react](https://reactjs.org/) 实现
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/admin/debug.vue
@@ -0,0 +1,40 @@
+<template>
+<div id="content" class="main">
+  <div class="form-group">
+    <button type="button" @click="debug('populate')" class="btn btn-primary">populate</button>
+  </div>
+  <div class="form-group">
+    <button type="button" @click="debug('reset_db')" class="btn btn-primary">reset db</button>
+  </div>
+</div>
+</template>
+
+<script>
+import { fetch } from 'src/utils'
+import { Notification } from 'element-ui'
+export default {
+  data () {
+    return {
+      loading: false
+    }
+  },
+  methods: {
+    debug (action) {
+      fetch({
+        method: 'get',
+        url: '/admin/debug/' + action
+      }).then((res) => {
+        Notification.success({title: 'Success', message: res.data})
+        this.loading = false
+      }).catch((err) => {
+        Notification.error({title: 'Error', message: err.response.data})
+        this.loading = false
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
